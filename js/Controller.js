bb450e332867173a4382f942864c913f0a9ba971
bemyhre <ben.myhre35@gmail.com>
2014-06-13 11:13:57 -0700

    edit hover things

--- js/Controller.js
+++ js/Controller.js
@@ -1,21 +1,26 @@
 
 window.onload = function() {
   var view = new View()
+  var hoverList = new Hoverview() 
   var model = new Model()
-  var controller = new Controller(view,model)
+  var controller = new Controller(view,model, hoverList)
   controller.bindListeners()
 }
 
 
-function Controller(display,model) {
+function Controller(display,model,hoverable) {
   this.display = display;
   this.model = model;
+  this.hoverable = hoverable;
 }
 
 Controller.prototype = {
   bindListeners:  function() {
     var previous= this.display.getPreviousArrow()
     var next = this.display.getNextArrow()
+    var hoverList = this.hoverable.getAllList()
+    hoverList.mouseover(this.hoverable.displayTheDeets)
+    hoverList.mouseout(this.hoverable.originalDeets)
     previous.addEventListener('click',this.moveBack.bind(this))
     next.addEventListener('click',this.moveAhead.bind(this))
   },
